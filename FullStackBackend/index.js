82a366786c959cb895ab27f55776cd5b774cd8d7
Juho Suni <juho.suni@protonmail.com>
2020-06-26 19:57:54 +0300

    Init Commit

--- FullStackBackend/index.js
+++ FullStackBackend/index.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const morgan = require('morgan')
 const app = express()
+const cors = require('cors')
+app.use(cors())
 
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+
 
 //--------------------------------
 //------------ MORGAN ------------
