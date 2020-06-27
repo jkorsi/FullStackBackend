163243b0c0439bea04ef17d2abbc8a96d914f8dc
Juho Suni <juho.suni@protonmail.com>
2020-06-27 12:18:23 +0300

    Heroku Changes

--- FullStackBackend/README.md
+++ FullStackBackend/README.md
@@ -1,2 +1,4 @@
 # FullStackBackend
 Repository for backend of the Open Full Stack Course
+HEROKU-APP:
+https://hy-numbers.herokuapp.com/api/persons
--- FullStackBackend/index.js
+++ FullStackBackend/index.js
@@ -5,6 +5,8 @@ const cors = require('cors')
 app.use(cors())
 
 app.use(express.json())
+app.use(express.static('build'))
+
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 //--------------------------------
