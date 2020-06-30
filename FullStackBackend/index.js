5e6879330261b3ad2440912c3d508a51bdf93392
Juho Suni <juho.suni@protonmail.com>
2020-06-30 13:38:33 +0300

    Initial frontend build added

--- FullStackBackend/index.js
+++ FullStackBackend/index.js
@@ -9,6 +9,7 @@ app.use(express.static('build'))
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
+
 //--------------------------------
 //------------ MORGAN ------------
 //--------------------------------
